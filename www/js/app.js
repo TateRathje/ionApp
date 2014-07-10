9ed8eb36768f30bff5955a0bd3539921ddb123e2
Tate Rathje <taterathje@gmail.com>
2014-07-10 03:09:14 -0700

    Added a button and icon in the top-right corner of header.

--- www/js/app.js
+++ www/js/app.js
@@ -3,5 +3,7 @@ var App = angular.module("App", ["ionic"]);
 App.controller("AppCtrl", ["$scope", "$log", AppCtrl]);
 
 function AppCtrl($scope, $log) {
-	
+	$scope.refresh = function() {
+		alert("Button pressed");
+	}
 }
